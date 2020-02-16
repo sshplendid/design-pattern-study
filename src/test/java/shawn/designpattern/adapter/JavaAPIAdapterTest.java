5443ef028899c23a50b7322e6b5e9ec181f590bb
SeHun Shin <s.shin@marathoner.kr>
2020-02-16 20:42:24 +0900

    test: 테스트 케이스(java api) 추가

--- src/test/java/shawn/designpattern/adapter/JavaAPIAdapterTest.java
+++ src/test/java/shawn/designpattern/adapter/JavaAPIAdapterTest.java
@@ -6,8 +6,8 @@ import org.junit.jupiter.api.Test;
 import java.io.*;
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
+import static java.util.stream.Collectors.joining;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class JavaAPIAdapterTest {
@@ -21,26 +21,57 @@ public class JavaAPIAdapterTest {
     }
 
     @Test
-    @DisplayName("InputStream 객체를 받아 Reader 객체를 반환하는 InputStreamReader")
-    void inputStreamReaderTest() {
+    @DisplayName("File 객체를 받아 Reader 객체를 반환하는 BufferedReader")
+    void bufferedReaderTest() {
         // GIVEN
         final String projectPath = System.getProperty("user.dir");
         final String relativePath = "/build/resources/test/hello.txt";
 
         final File hello = new File(projectPath + relativePath);
 
-        try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(new FileInputStream(hello)))) {
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(hello))) {
 
             // WHEN
-            String contents = bufferedReader.lines().collect(Collectors.joining("\n"));
+            String contents = bufferedReader.lines().collect(joining("\n"));
 
             // THEN
             assertThat(contents).isNotNull();
             assertThat(contents).isEqualTo("Hello, World!");
+            System.out.println(contents);
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    @Test
+    void inputStreamTest() {
+        // GIVEN
+        final String projectPath = System.getProperty("user.dir");
+        final String relativePath = "/build/resources/test/hello.txt";
+        final File file = new File(projectPath + relativePath);
+
+        // WHEN
+        try (FileInputStream fileInputStream = new FileInputStream(file)) {
+            byte[] bytes = new byte[fileInputStream.available()];
+            int i = 0;
+
+            while(fileInputStream.available() > 0) {
+                bytes[i++] = (byte) fileInputStream.read();
+            }
+
+            String string = new String(bytes);
+            System.out.println(string);
+            for (byte b : bytes) System.out.print(b + ", ");
+            System.out.println();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        // THEN
     }
 }
